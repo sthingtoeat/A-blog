1349545db69aee0f6257691d6d277389f75d380e
sthingtoeat <1040606497@qq.com>
2024-02-27 17:01:49 +0800

    新增了评论功能，但是仅一个页面才有

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,34 +12,39 @@ module.exports = {
       ['meta', { name: 'theme-color', content: '#3eaf7c' }],
       ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
       ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-      ['link', { rel: 'apple-touch-icon', href: '/icons/icon512_rounded.png' }],
-      ['link', { rel: 'mask-icon', href: '/icons/icon512_maskable.png', color: '#3eaf7c' }],
-      ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+      ['link', { rel: 'apple-touch-icon', href: '/icons/icon512_rounded.png' }],              //这里的地址
+      ['link', { rel: 'mask-icon', href: '/icons/icon512_maskable.png', color: '#3eaf7c' }],  //还有这里都需要有文件才行
+      ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],//这里好像不用
       ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
-    plugins: [              //这里是插件，别放进themeConfig里面
-      [
-        '@vuepress/last-updated',
-        {
+    plugins: {              //这里是插件，别放进themeConfig里面，以前的插件形式为babel形式，现改为对象的形式，注意区别
+        '@vuepress/last-updated':{
           transformer: (timestamp, lang) => {
                                                 
             moment.locale(lang)                 // 不要忘了安装 moment
             // moment.locale("zh-cn");             //可以将英文转化为中文，使用时请禁用上一条代码，并删除lang
             return moment(timestamp).format("LLLL")  //其他格式请去moment官网寻找
           }
-        }
-      ],
-      [ 
-        '@vuepress/pwa', 
-        {
-          serviceWorker: true,
+        }, 
+        '@vuepress/pwa':{                        //pwa配置什么的请参考教程，具体是需要manifest.json文件
+          serviceWorker: true,                   //可通过网上的生成器生成，注意各参数的即可
           updatePopup: {
             message: "New content is available.",
             buttonText: "Refresh"
           }
-        }
-      ]
-    ],
+        },
+        '@vssue/vuepress-plugin-vssue': {
+          // 设置 `platform` 而不是 `api`
+          platform: 'github-v4',
+    
+          // 其他的 Vssue 配置
+          owner: 'sthingtoeat',                 //你github的名
+          repo: 'A-blog',                       //你这个项目的仓库名
+          clientId: '4e7f48211858da0895e9',
+          clientSecret: 'f728ac46b50aaa4dc0b4e39dfc23d9c7c10823e1', //这东西只生成一次，忘记了就得重置
+          autoCreateIssue:true,                 //自动创建评论(create issue)，不用手点了
+        },
+      },
     themeConfig: {
       lastUpdated: 'updateDate',        //以git上传的时间为标准,但是格式改不了，需要插件
       logo: '/assets/img/logo.png',
--- docs/README.md
+++ docs/README.md
@@ -5,3 +5,6 @@ title: Start
 ## Hello VuePress 
 ### 这里是vuepress的默认路径，地址为空时就会跳转到这里
 ### index.md文件等效为README.md文件
+
+<!-- 下面这个评论功能只添加在本页面，其他页面则没有的，想布置全局的话请参考教程 -->
+<Vssue :options="{locale:'zh'}"/>
