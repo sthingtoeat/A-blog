533cc6b146104a04eb4b6f07d07ccd4e63664c5e
sthingtoeat <1040606497@qq.com>
2024-03-07 21:11:57 +0800

    测试ignore

---
+++ docs/.vuepress/config/secret.js
@@ -0,0 +1,4 @@
+module.exports={    //这里放置一些隐私信息,注意配置.gitignore文件，防止该文件被上传
+    clientId: '4e7f48211858da0895e9',
+    clientSecret: 'f728ac46b50aaa4dc0b4e39dfc23d9c7c10823e1', //这东西只生成一次，忘记了就得重置
+}
